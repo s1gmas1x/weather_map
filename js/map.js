6f72234f77b1d0563c76c2502ae18f69f72a6f98
s1gmas1x <sigma_vi@yahoo.com>
2021-10-22 14:53:41 -0600

    changed_var_to_const

--- js/map.js
+++ js/map.js
@@ -1,81 +1,109 @@
-var mymap = L.map('mapid', { minZoom:3 }).setView([32, -64], 4);
-	
-L.esri.basemapLayer('DarkGray').addTo(mymap);
-L.esri.basemapLayer('DarkGrayLabels').addTo(mymap);
+var gray = L.layerGroup();
+var img = L.layerGroup();
 
-var southWest = L.latLng(-90, -180),
-northEast = L.latLng(90, 180);
+L.esri.basemapLayer('DarkGray').addTo(gray);
+L.esri.basemapLayer('DarkGrayLabels').addTo(gray);
+L.esri.basemapLayer('Imagery').addTo(img);
+L.esri.basemapLayer('ImageryLabels').addTo(img);
 
-var bounds = L.latLngBounds(southWest, northEast);
 
-mymap.setMaxBounds(bounds);
 
-mymap.on('drag', function() {
-    mymap.panInsideBounds(bounds, { animate: false });
-});
-
-L.control.scale().addTo(mymap);
 
 // NOAA nexrad radar layer
- L.esri.dynamicMapLayer({
+var radar = L.esri.dynamicMapLayer({
     url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
-  }).addTo(mymap);
+});
 // end NOAA nexrad radar
 
 // NWS moderate events layer
-L.esri.featureLayer({
+var moderate = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/10',
-	 style: function (feature) {
-      return { color: '#6aaf00', weight: .125 };
+    style: function(feature) {
+        return { color: '#6aaf00', weight: .125 };
     }
-  }).addTo(mymap);
+});
 // end moderate events
 
 // NWS severe events layer
-L.esri.featureLayer({
+var severe = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/9',
-	 style: function (feature) {
-      return { color: '#cccc00', weight: 1 };
+    style: function(feature) {
+        return { color: '#cccc00', weight: 1 };
     }
-  }).addTo(mymap);
+});
 // end severe events
 
 // NWS extreme events layer
-L.esri.featureLayer({
+var extreme = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/8',
-	 style: function (feature) {
-      return { color: '#ff5500', weight: 2 };
+    style: function(feature) {
+        return { color: '#ff5500', weight: 2 };
     }
-  }).addTo(mymap);
+});
 // end extreme events
 
 // hurricane forcast error zone layer
-L.esri.featureLayer({
+var caneError = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/4',
-	 style: function (feature) {
-      return { color: '#6aaf00', weight: .125 };
+    style: function(feature) {
+        return { color: '#6aaf00', weight: .125 };
     }
-  }).addTo(mymap);
+});
 // end error zone
 
 // hurricane forcast track layer
-L.esri.featureLayer({
+var caneCast = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/2',
-	 style: function (feature) {
-      return { color: '#cccc00', weight: 1 };
+    style: function(feature) {
+        return { color: '#cccc00', weight: 1 };
     }
-  }).addTo(mymap);
+});
 // end forcast track
 
 // hurricane observed track layer
-L.esri.featureLayer({
+var caneTrack = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/3',
-	 style: function (feature) {
-      return { color: '#ff5500', weight: 2 };
+    style: function(feature) {
+        return { color: '#ff5500', weight: 2 };
     }
-  }).addTo(mymap);
+});
 // end observed track
+var map = L.map('mapid', {
+
+    center: [32, -64],
+    zoom: 4,
+    minZoom: 3,
+    layers: [gray, radar, extreme, severe, moderate, caneError, caneCast, caneTrack]
+});
+
+var southWest = L.latLng(-90, -180),
+    northEast = L.latLng(90, 180);
 
 
+var bounds = L.latLngBounds(southWest, northEast);
 
+map.setMaxBounds(bounds);
 
+map.on('drag', function() {
+    map.panInsideBounds(bounds);
+});
+
+var baseLayers = {
+    Grayscale: gray,
+    Streetmap: L.esri.basemapLayer('Streets'),
+    Image: img
+};
+
+var overlays = {
+    'Radar': radar,
+    'NWS Extreme Events': extreme,
+    'NWS Severe Events': severe,
+    'NWS Moderate Events': moderate,
+    'Hurricane Error Cone': caneError,
+    'Hurricane Forecast Track': caneCast,
+    'Hurricane Observed Track': caneTrack
+
+};
+
+L.control.scale().addTo(map);
+L.control.layers(baseLayers, overlays).addTo(map);
