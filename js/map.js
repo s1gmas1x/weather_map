093374f5472dc85e188749ae02617a7cb025e27b
s1gmas1x <sigma_vi@yahoo.com>
2021-11-17 12:06:57 -0700

    fixed_margins

--- js/map.js
+++ js/map.js
@@ -20,54 +20,42 @@ const radar = L.esri.dynamicMapLayer({
 // NWS moderate events layer
 const moderate = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/10',
-    style: function(feature) {
-        return { color: '#6aaf00', weight: .125 };
-    }
+    style: () => ({ color: '#6aaf00', weight: .125 })
 });
 // end moderate events
 
 // NWS severe events layer
 const severe = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/9',
-    style: function(feature) {
-        return { color: '#cccc00', weight: 1 };
-    }
+    style: () => ({ color: '#cccc00', weight: 1 })
 });
 // end severe events
 
 // NWS extreme events layer
 const extreme = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/8',
-    style: function(feature) {
-        return { color: '#ff5500', weight: 2 };
-    }
+    style: () => ({ color: '#ff5500', weight: 2 })
 });
 // end extreme events
 
-// hurricane forcast error zone layer
+// hurricane forcast error cone layer
 const caneError = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/4',
-    style: function(feature) {
-        return { color: '#6aaf00', weight: .125 };
-    }
+    style: () => ({ color: '#6aaf00', weight: .125 })
 });
-// end error zone
+// end error cone
 
 // hurricane forcast track layer
 const caneCast = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/2',
-    style: function(feature) {
-        return { color: '#cccc00', weight: 1 };
-    }
+    style: () => ({ color: '#cccc00', weight: 1 })
 });
 // end forcast track
 
 // hurricane observed track layer
 const caneTrack = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/3',
-    style: function(feature) {
-        return { color: '#ff5500', weight: 2 };
-    }
+    style: () => ({ color: '#ff5500', weight: 2 })
 });
 // end observed track
 map = L.map('mapid', {
