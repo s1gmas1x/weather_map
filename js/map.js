057598e514207e8f3d64d98829a8b0c55f7f818e
s1gmas1x <sigma_vi@yahoo.com>
2022-10-31 11:54:08 -0600

    updated_basemaps

--- js/map.js
+++ js/map.js
@@ -1,12 +1,16 @@
+
+
 const gray = L.layerGroup();
 const img = L.layerGroup();
 const street = L.layerGroup();
+const apiKey = "AAPKd0aca9e07fc246f89c572149904824b5zqJMhMutQv6gAiZ2W6AMZ9yimm-86BRon5zxfe0sBYDwdqxuMaSW3wbeuGJqnFzB";
+  
 
-L.esri.basemapLayer('DarkGray').addTo(gray);
-L.esri.basemapLayer('DarkGrayLabels').addTo(gray);
-L.esri.basemapLayer('Streets').addTo(street);
-L.esri.basemapLayer('Imagery').addTo(img);
-L.esri.basemapLayer('ImageryLabels').addTo(img);
+//L.esri.basemapLayer('DarkGray').addTo(gray);
+//L.esri.basemapLayer('DarkGrayLabels').addTo(gray);
+//L.esri.basemapLayer('Streets').addTo(street);
+//L.esri.basemapLayer('Imagery').addTo(img);
+//L.esri.basemapLayer('ImageryLabels').addTo(img);
 
 
 
@@ -20,50 +24,64 @@ const radar = L.esri.dynamicMapLayer({
 // NWS moderate events layer
 const moderate = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/10',
-    style: () => ({ color: '#6aaf00', weight: .125 })
+    style: function() {
+        return { color: '#6aaf00', weight: .125 };
+    }
 });
+
 // end moderate events
 
 // NWS severe events layer
 const severe = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/9',
-    style: () => ({ color: '#cccc00', weight: 1 })
+    style: function() {
+        return { color: '#cccc00', weight: 1 };
+    }
 });
 // end severe events
 
-// NWS extreme events layer
+    // NWS extreme events layer
 const extreme = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/NWS_Watches_Warnings_v1/FeatureServer/8',
-    style: () => ({ color: '#ff5500', weight: 2 })
+    style: function() {
+        return { color: '#ff5500', weight: 2 };
+    }
 });
 // end extreme events
 
-// hurricane forcast error cone layer
+// hurricane forcast error zone layer
 const caneError = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/4',
-    style: () => ({ color: '#6aaf00', weight: .125 })
+    style: function() {
+        return { color: '#6aaf00', weight: .125 };
+    }
 });
-// end error cone
+// end error zone
 
 // hurricane forcast track layer
 const caneCast = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/2',
-    style: () => ({ color: '#cccc00', weight: 1 })
+    style: function() {
+        return { color: '#cccc00', weight: 1 };
+    }
 });
 // end forcast track
 
 // hurricane observed track layer
 const caneTrack = L.esri.featureLayer({
     url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Active_Hurricanes_v1/FeatureServer/3',
-    style: () => ({ color: '#ff5500', weight: 2 })
+    style: function() {
+        return { color: '#ff5500', weight: 2 };
+    }
 });
 // end observed track
+
 map = L.map('mapid', {
 
     center: [32, -64],
     zoom: 4,
     minZoom: 3,
-    layers: [gray, radar, extreme, severe, moderate, caneError, caneCast, caneTrack],
+    layers: [gray, radar, moderate, severe, extreme, caneError, caneCast, caneTrack],
     maxBoundsViscosity: 1.0,
 });
 
@@ -75,16 +93,21 @@ map.setMaxBounds([
 
 
 const baseLayers = {
-    'Grayscale': gray,
-    'Street Map': street,
-    'Image': img
+    "Dark gray": L.esri.Vector.vectorBasemapLayer("ArcGIS:DarkGray", { apiKey: apiKey }).addTo(map),
+    
+    Streets: L.esri.Vector.vectorBasemapLayer("ArcGIS:Streets", { apiKey: apiKey }),
+    Navigation: L.esri.Vector.vectorBasemapLayer("ArcGIS:Navigation", { apiKey: apiKey }),
+    Topographic: L.esri.Vector.vectorBasemapLayer("ArcGIS:Topographic", { apiKey: apiKey }),
+    Imagery: L.esri.Vector.vectorBasemapLayer("ArcGIS:Imagery", { apiKey: apiKey }),
+    ChartedTerritory: L.esri.Vector.vectorBasemapLayer("ArcGIS:ChartedTerritory", { apiKey: apiKey }),
+    Nova: L.esri.Vector.vectorBasemapLayer("ArcGIS:Nova", { apiKey: apiKey }),
 };
 
 const overlays = {
     'Radar': radar,
-    'NWS Extreme Events': extreme,
-    'NWS Severe Events': severe,
     'NWS Moderate Events': moderate,
+    'NWS Severe Events': severe,
+    'NWS Extreme Events': extreme,
     'Hurricane Error Cone': caneError,
     'Hurricane Forecast Track': caneCast,
     'Hurricane Observed Track': caneTrack
